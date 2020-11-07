d27e9f5e72a4de53075b4dc9c8cce1beb2d5218e
Zenitsu392 <74070979+Zenitsu392@users.noreply.github.com>
2020-11-06 21:06:22 -0500

    Add files via upload

---
+++ GitHub Proposal.py
@@ -0,0 +1,15 @@
+#Proposal
+
+"We are thinking of creating a game."
+
+# What idea(s) do you have for your final project?
+
+"We are planning to do a remake of the game snake using our own twist and take on it"
+
+# If you plan to collaborate with one or two classmates, what are their names?
+
+"Michael Zheng & Lucky Ly"
+
+# Do you have any questions of your own?
+
+"Is there a rubric regarding for what's required on the final Project?"
