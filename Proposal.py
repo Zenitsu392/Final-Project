227f2e5df0c0670bcd9c25c220f6fd01627c1db1
Zenitsu392 <74070979+Zenitsu392@users.noreply.github.com>
2020-11-16 15:41:42 -0500

    Add files via upload

---
+++ Proposal.py
@@ -0,0 +1,42 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+The Title of our project is Python Project aka Ekins.
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+The game of snake with our twist and take on it.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+We will do Python programming as our main source of software. The features of our twist and take consist of poisinous apples being included then just having plain old apples for the player's awareness.
+The score will be depending on the amount of  regular apples consumed. If poisin apples are consumed the player's score willl suffer. Along with the score, the size of the snake will either increase or decrease
+depending on it's consumption. Other powers up may be applicable in adding to this project.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+We will accomplish the game of snake with the feature of poisinous apples no matter what. 
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+ 1 Power ups of the snake can be included to give the player a better overall score. 
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+More power ups or power downs are being added to either give the player a better or worsen their score. Also not having the apples spawn at the same time but randomly. 
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+Since we are working as a trio, we will try to split the work as evenly as possible. Our first steps would be to outline the sections needed to put the game snake together.
+the scoreboard, the different types of apples to add in, the snake itself and its movement and speed along with the background as well. 
