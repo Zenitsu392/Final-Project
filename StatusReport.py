f35a5531458fec3a108bc422641b2a95306a788b
Zenitsu392 <74070979+Zenitsu392@users.noreply.github.com>
2020-12-03 20:53:48 -0500

    Add files via upload

---
+++ StatusReport.py
@@ -0,0 +1,29 @@
+# Status Report
+
+#### Your name
+
+Brandon Le
+
+#### Your section leader's name
+
+Joanne Nichols
+
+#### Project title
+
+Snake 2.0 or Ekans (we did not really decide what the game would be called yet)
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+
+We have created the basic credentials needed to create the snake game so far
+
+#### What have you not done for your project yet?
+
+We still need to add the poison apple and golden apple. If we have enough time, maybe another power up would be ideal.
+
+#### What problems, if any, have you encountered?
+
+We have yet to know how to randomize the golden apple or other apples so that it does not show up everytime the snake eats consumes food.
